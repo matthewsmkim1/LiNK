2a604859c298410525ddffe7eeb00bad6ba82fa7
Matt <Matthew.sm.kim1@gmail.com>
2020-04-01 14:03:55 -0500

    added bottom nav bar

---
+++ LINK/app/src/main/java/com/example/link/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.link
+
+import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var textView: TextView
+
+    //This is the listener for our bottom navigation bar
+    //We access is in MainActivity onCreate I believe
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.nav_home -> {
+                textView.setText("Home")
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_search -> {
+                textView.setText("Search")
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_add_post -> {
+                textView.setText("Add Post")
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_notifications -> {
+                textView.setText("Notifications")
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_profile -> {
+                textView.setText("Profile")
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        textView = findViewById(R.id.message)
+
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+
+
+    }
+
+
+}
--- dummy_backend/server/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  PORT: 8080,
-  MONGODB_URI: "mongodb://mongoDB:27017/restifyApp",
-  ENV: 'development',
-  URL: 'http://localhost:8080',
-}
