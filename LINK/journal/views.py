5e625012c607438672c2d44e945ce514abe6e1f4
Matt <Matthew.sm.kim1@gmail.com>
2020-04-08 16:22:07 -0500

    added templates for home page and about page

--- LINK/journal/views.py
+++ LINK/journal/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Journal Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Journal Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'journal/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'journal/about.html', {'title': 'About'})
