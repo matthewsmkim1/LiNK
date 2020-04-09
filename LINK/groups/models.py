800ff5d1e28a98c4af6cdeb491d5cd72b0f5bb57
golden-mango <mtdeppe75@gmail.com>
2020-04-09 14:16:20 -0400

    create a group done

--- LINK/groups/models.py
+++ LINK/groups/models.py
@@ -7,6 +7,8 @@ from django.utils import timezone
 
 from django.contrib.auth.models import Group
 from django.contrib.auth.hashers import BCryptSHA256PasswordHasher as bcrypt
+from PIL import Image
+
 # Create your models here.
 
 
@@ -14,14 +16,17 @@ from django.contrib.auth.hashers import BCryptSHA256PasswordHasher as bcrypt
 #! right now when adding someone to the group they need to present the bkey, since django cannot filter based on binaryfield I hacked in
 #! it filtering based on the group_name --> this means can only have unique group names for now
 class LinkGroup(models.Model):
-    group = models.OneToOneField(Group, on_delete=models.CASCADE)
     group_name = models.CharField(max_length=200)
     group_description = models.CharField(max_length=2000, default="New Group!")
     date_created = models.DateTimeField(default=timezone.now)
     members = models.ManyToManyField(Profile)
     posts = models.ManyToManyField(Post)
     bkey = models.BinaryField()  # this must be hashed using bycrpt to a b"" string
-    # todo add calendar and announcments to a group
+    group_avatar = models.ImageField(
+        default='default.jpg', upload_to='profile_pics')
+
+    # todo add calendar, announcements, and have group creator be an admin
+    # admin =
     # calender =
     # announcments =
 
@@ -30,3 +35,13 @@ class LinkGroup(models.Model):
 
     # def get_absolute_url(self):
     #     return reverse('group-detail', kwargs={'pk': self.pk})
+
+    def save(self, *args, **kwargs):
+        super(LinkGroup, self).save(*args, **kwargs)
+
+        img = Image.open(self.group_avatar.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
--- LINK/groups/views.py
+++ LINK/groups/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 from django.contrib.auth.models import User
 from django.views.generic import (
     ListView,
@@ -9,4 +12,113 @@ from django.views.generic import (
     DeleteView
 )
 from .models import LinkGroup
+from users.models import Profile
+from .forms import LinkGroupCreationForm, LinkGroupJoinForm
 # Create your views here.
+import bcrypt
+import random
+import string
+
+
+def randomString(stringLength=50):
+    """Generate a random string of fixed length """
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+
+@login_required
+def create_group(request):
+    current_user = request.user.username
+    if request.method == "POST":
+        group_creation_form = LinkGroupCreationForm(request.POST)
+        if group_creation_form.is_valid():
+            salt = bcrypt.gensalt()
+            key = randomString()
+            encoded = key.encode()
+            hashed = bcrypt.hashpw(encoded, salt)
+            group_creation_form.bkey = hashed
+            group_creation_form.members.add(request.user)
+            group_creation_form.save()
+            messages.success(
+                request, f"This is your group's join key. Make sure to keep it somewhere safe! \n {key} \n \
+                 If you want to add someone to the group, give them the key and the group name\n {group_creation_form.cleaned_data['group_name']}")
+            return redirect('profile')
+
+        # return redirect('/show_group)
+    else:
+        group_creation_form = LinkGroupCreationForm()
+        context = {
+            "group_creation_form": group_creation_form
+        }
+        return render(request, 'groups/create_group.html', context)
+
+#! TODO
+@login_required
+def show_group(request, groupname):
+    current_user = request.user.username
+
+
+#! TODO
+@login_required
+def add_post_to_group(request, groupname):
+    current_user = request.user.username
+
+#! TODO
+@login_required
+def create_post_in_group(request, groupname):
+    current_user = request.user.username
+
+
+def add_user_to_group(user, group):
+    profile = Profile.objects.get(user=user)
+    group.members.add(user)
+
+
+@login_required
+def join_group(request):
+    current_user = request.user.username
+    if request.method == "POST":
+        group_join_form = LinkGroupJoinForm(request.POST)
+        if group_join_form.is_valid():
+            key = group_join_form.cleaned_data['bkey']
+            group_name = group_join_form.cleaned_data['group_name']
+            group = LinkGroup.objects.get(group_name=group_name)
+
+            if bcrypt.checkpw(key.encoded(), group.bkey):
+                add_user_to_group(request.user, group)
+                messages.success(
+                    request, f"You have been successfully added to the group {group_name}!")
+                return redirect('profile')
+            else:
+                messages.failure(request, f"Sorry, that key is not correct")
+                group_join_form = LinkGroupJoinForm()
+                context = {
+                    "group_join_form": group_join_form
+                }
+                return render(request, 'groups/join_group.html', context)
+
+        # return redirect('/show_group)
+    else:
+        group_join_form = LinkGroupJoinForm()
+        context = {
+            "group_join_form": group_join_form
+        }
+        return render(request, 'groups/join_group.html', context)
+
+
+#! TODO after admin functionality is added to group so the admin can change things, such as the group avatar
+@login_required
+def edit_group_details(request):
+    current_user = request.user.username
+
+
+#! TODO after admin functionality is added to group so the admin can change things, such as the group avatar
+@login_required
+def edit_group_announcements(request):
+    current_user = request.user.username
+
+
+#! TODO after admin functionality is added to group so the admin can change things, such as the group avatar
+@login_required
+def edit_group_calendar(request):
+    current_user = request.user.username
